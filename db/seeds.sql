03b2b61a2b44da797045bfc6ccc6ceabafeaa4af
Malorie Iglesias <malorieiglesias@yahoo.com>
2019-01-28 21:43:24 -0700

    Package JSON correction

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Jalapeno and Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Bacon and Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Whataburger');
+INSERT INTO burgers (burger_name) VALUES ('Whataburger Jr');
+INSERT INTO burgers (burger_name) VALUES ('Double Meat Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Triple Meat Burger', true);
