3c5d92e7aff8644d2b601d88640aab1d87829db5
Malorie Iglesias <malorieiglesias@yahoo.com>
2019-01-31 22:00:26 -0700

    fixed server.js file

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-// var methodOverride = require('method-override');
+var methodOverride = require('method-override');
 
 
 var app = express();
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({
   extended: false
 }));
 // override with POST having ?_method=DELETE 
-// app.use(methodOverride('_method'));
+app.use(methodOverride('_method'));
 var exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({
   defaultLayout: 'main'
